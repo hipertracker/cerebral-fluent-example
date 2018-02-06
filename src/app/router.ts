bceb1c5827d3af595e96e30bd900320680d3c1a9
hipertracker <hipertracker@gmail.com>
2018-02-06 13:52:21 +0000

    remove out of date comments

--- src/app/router.ts
+++ src/app/router.ts
@@ -6,13 +6,7 @@ const router = Router({
   routes: [
     { path: '/', signal: 'homeRouted' },
     { path: '/search', signal: 'searchRouted' },
-
-    /*    
-    Error: Cerebral - Signal with name "read" is not correctly defined. 
-    Please check that the signal is either a sequence, an array or a function.
-    */
     { path: '/read', signal: 'read.routed' },
-
     { path: '/help', signal: 'helpRouted' },
     { path: '/history', signal: 'historyRouted' },
     { path: '/config', signal: 'configRouted' },
